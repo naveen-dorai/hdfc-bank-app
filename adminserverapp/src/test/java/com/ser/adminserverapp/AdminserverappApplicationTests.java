acfcc71b9319932e4129fb1752aba0f0fd79db9a
naveen <naveen.dmswamy@gmail.com>
2024-05-25 16:26:01 +0530

    initial commit

---
+++ adminserverapp/src/test/java/com/ser/adminserverapp/AdminserverappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ser.adminserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminserverappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
